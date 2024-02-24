3e5333000ffa099d3b0c7f7509a0ff4913e0e053
Filipe Cavalcante <filipe.khalifa@gmail.com>
2024-02-23 22:19:34 -0300

    subindo projeto iniciado

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
