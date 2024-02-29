97fa8eef5c91e9f0d9146376a18f5c1a641a0153
Filipe Cavalcante <filipe.khalifa@gmail.com>
2024-02-29 08:54:26 -0300

    Adicionando funcionalidades de exclusão e o projeto mobile

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,8 +2,10 @@ import { UserPhoto } from './../interface/userPhoto';
 import { Injectable } from '@angular/core';
 
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
 	providedIn: 'root'
@@ -12,8 +14,13 @@ export class PhotoService {
 
 	public photos: UserPhoto[] = [];
 	private PHOTO_STORAGE: string = 'photos';
+	private platform: Platform;
 
-	constructor() { }
+	constructor(
+		platform: Platform
+	) {
+		this.platform = platform;
+	}
 
 	public async addNewToGallery() {
 		// Take a photo
@@ -32,32 +39,54 @@ export class PhotoService {
 		});
 	}
 
-	private async savePicture(photo: Photo) {
-		// Convert photo to base64 format, required by Filesystem API to save
-		const base64Data = await this.readAsBase64(photo);
+// Save picture to file on device
+private async savePicture(photo: Photo) {
+	// Convert photo to base64 format, required by Filesystem API to save
+	const base64Data = await this.readAsBase64(photo);
 
-		// Write the file to the data directory
-		const fileName = Date.now() + '.jpeg';
-		const savedFile = await Filesystem.writeFile({
-			path: fileName,
-			data: base64Data,
-			directory: Directory.Data
-		});
+	// Write the file to the data directory
+	const fileName = Date.now() + '.jpeg';
+	const savedFile = await Filesystem.writeFile({
+	  path: fileName,
+	  data: base64Data,
+	  directory: Directory.Data
+	});
 
-		// Use webPath to display the new image instead of base64 since it's
-		// already loaded into memory
-		return {
-			filepath: fileName,
-			webviewPath: photo.webPath
-		};
+	if (this.platform.is('hybrid')) {
+	  // Display the new image by rewriting the 'file://' path to HTTP
+	  // Details: https://ionicframework.com/docs/building/webview#file-protocol
+	  return {
+		 filepath: savedFile.uri,
+		 webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+	  };
 	}
+	else {
+	  // Use webPath to display the new image instead of base64 since it's
+	  // already loaded into memory
+	  return {
+		 filepath: fileName,
+		 webviewPath: photo.webPath
+	  };
+	}
+ }
 
 	private async readAsBase64(photo: Photo) {
-		// Fetch the photo, read as a blob, then convert to base64 format
-		const response = await fetch(photo.webPath!);
-		const blob = await response.blob();
+		// "hybrid" will detect Cordova or Capacitor
+		if (this.platform.is('hybrid')) {
+			// Read the file into base64 format
+			const file = await Filesystem.readFile({
+				path: photo.path!
+			});
 
-		return await this.convertBlobToBase64(blob) as string;
+			return file.data;
+		}
+		else {
+			// Fetch the photo, read as a blob, then convert to base64 format
+			const response = await fetch(photo.webPath!);
+			const blob = await response.blob();
+
+			return await this.convertBlobToBase64(blob) as string;
+		}
 	}
 
 	private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
@@ -74,16 +103,40 @@ export class PhotoService {
 		const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
 		this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
 
-		// Display the photo by reading into base64 format
-		for (let photo of this.photos) {
-			// Read each saved photo's data from the Filesystem
-			const readFile = await Filesystem.readFile({
-				path: photo.filepath,
-				directory: Directory.Data,
-			});
+		// Easiest way to detect when running on the web:
+		// “when the platform is NOT hybrid, do this”
+		if (!this.platform.is('hybrid')) {
+		  // Display the photo by reading into base64 format
+		  for (let photo of this.photos) {
+			 // Read each saved photo's data from the Filesystem
+			 const readFile = await Filesystem.readFile({
+				  path: photo.filepath,
+				  directory: Directory.Data
+			 });
 
-			// Web platform only: Load the photo as base64 data
-			photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+			 // Web platform only: Load the photo as base64 data
+			 photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+		  }
 		}
-	}
+	 }
+
+	 public async deletePicture(photo: UserPhoto, position: number) {
+		// Remove this photo from the Photos reference data array
+		this.photos.splice(position, 1);
+
+		// Update photos array cache by overwriting the existing photo array
+		Preferences.set({
+		  key: this.PHOTO_STORAGE,
+		  value: JSON.stringify(this.photos)
+		});
+
+		// delete photo file from filesystem
+		const filename = photo.filepath
+								  .substr(photo.filepath.lastIndexOf('/') + 1);
+
+		await Filesystem.deleteFile({
+		  path: filename,
+		  directory: Directory.Data
+		});
+	 }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto } from '../interface/userPhoto';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +11,8 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page implements OnInit {
 
   constructor(
-    public photoService: PhotoService
+    public photoService: PhotoService,
+	 public actionSheetController: ActionSheetController
   ) {}
 
   addPhotoToGallery() {
@@ -19,5 +22,27 @@ export class Tab2Page implements OnInit {
   async ngOnInit() {
 	 await this.photoService.loadSaved();
   }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+	const actionSheet = await this.actionSheetController.create({
+	  header: 'Photos',
+	  buttons: [{
+		 text: 'Delete',
+		 role: 'destructive',
+		 icon: 'trash',
+		 handler: () => {
+			this.photoService.deletePicture(photo, position);
+		 }
+	  }, {
+		 text: 'Cancel',
+		 icon: 'close',
+		 role: 'cancel',
+		 handler: () => {
+			// Nothing to do, action sheet is automatically closed
+			}
+	  }]
+	});
+	await actionSheet.present();
+ }
 
 }
